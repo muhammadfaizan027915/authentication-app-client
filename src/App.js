b23aeeaf5be48d057b6f5e5ed5c4181032523fb2
muhammadfaizan027915 <muhammadfaizan027915@gmail.com>
2023-02-03 21:32:45 +0500

    Auth app UI created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Layout from "./Pages/Layout";
+import Login from "./Pages/Login";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import Home from "./Pages/Home";
+import Register from "./Pages/Register";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route index element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+    </Route>
+  )
+);
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
 
 export default App;
---
+++ src/Pages/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div
+      style={{
+        height: "100vh",
+        width: "100vw",
+        marginTop: "200px",
+        textAlign: "center"
+      }}
+    >
+      <h1>Welcome to Auth App</h1>
+    </div>
+  );
+};
+
+export default Home;
